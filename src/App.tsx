702c05de38da8fae7a9b6ad83d1e551325bb8ab1
micjef <michaeljeffrysetiawan@gmail.com>
2023-08-20 22:08:31 +0700

    create signin and signup page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Navbar from "components/Navbar";
 import HomePage from "src/pages/Home";
 import MyIdeaPage from 'src/pages/MyIdea';
 import CreateIdeaPage from 'src/pages/CreateIdea';
+import SignInPage from 'src/pages/SignIn';
+import SignUpPage from 'src/pages/SignUp';
 
 function App() {
 	return (
@@ -13,6 +15,8 @@ function App() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="/my-idea" element={<MyIdeaPage />} />
 				<Route path="/create-idea" element={<CreateIdeaPage />} />
+				<Route path="/signin" element={<SignInPage />} />
+				<Route path="/signup" element={<SignUpPage />} />
 			</Routes>
 		</>
 	);
