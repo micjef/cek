5ed746cae1d70c8c0c2f69ada96e5e0328f63b62
micjef <michaeljeffrysetiawan@gmail.com>
2023-08-22 21:14:07 +0700

    create detail idea page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import MyIdeaPage from 'src/pages/MyIdea';
 import CreateIdeaPage from 'src/pages/CreateIdea';
 import SignInPage from 'src/pages/SignIn';
 import SignUpPage from 'src/pages/SignUp';
+import DetailIdeaPage from 'src/pages/DetailIdea';
 
 function App() {
 	return (
@@ -15,6 +16,7 @@ function App() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="/my-idea" element={<MyIdeaPage />} />
 				<Route path="/create-idea" element={<CreateIdeaPage />} />
+				<Route path="/detail-idea" element={<DetailIdeaPage />} />
 				<Route path="/signin" element={<SignInPage />} />
 				<Route path="/signup" element={<SignUpPage />} />
 			</Routes>
